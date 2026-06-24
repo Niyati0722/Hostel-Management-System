3cd91bb08e4b30939b4cadbde8962e3c3bb69f87
Niyati0722 <niyati.agrawal2612@gmail.com>
2026-06-24 19:10:13 +0530

    update API base URL for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://hosteliq-backend.onrender.com/api' });
 
 // Attach token to every request automatically
 API.interceptors.request.use((req) => {
